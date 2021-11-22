d8907a67f39ae016fffddce3a5f7d436ec9d6a3d
Vinod <vsramalwan@gmail.com>
2021-11-22 14:10:40 +0100

    fix: lint

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,7 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 module.exports = {
-    "arrowParens": "avoid",
-    "printWidth": 120,
-    "semi": false,
-    "singleQuote": true,
-    "tabWidth": 4
-}
+  arrowParens: 'avoid',
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 4,
+};
